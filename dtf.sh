b00289c183a31278b34d6cb3e701795803b9e032
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2026-02-21 22:19:34 -0600

    cleanup variables now that local is gone

--- dtf.sh
+++ dtf.sh
@@ -3,7 +3,7 @@
 # MIT License
 # Copyright (c) 2021 Jeffrey Clark <https://github.com/h0tw1r3>
 
-alias _dtf='/usr/bin/env git --git-dir="$HOME/.dtf" --work-tree="$HOME"'
+alias _dtf='/usr/bin/env git --git-dir="$_DTF_WORKDIR" --work-tree="$HOME"'
 
 _dtf_msg() {
     echo >&2 "${_DTF_FN}: $*"
@@ -19,42 +19,36 @@ _dtf_output_url() {
 
 dtf() {
     export _DTF_FN="dtf"
-    _CHECKS="${DTF_CHECKS:-1}"
-    _REPO="$HOME/.${_DTF_FN}"
-    _DTFURL="${DTF_URL:-https://github.com/h0tw1r3/dtf/raw/main/dtf.sh}"
-    _BRANCH="${DTF_BRANCH:-main}"
+    export _DTF_WORKDIR="$HOME/.${_DTF_FN}"
+    DTF_CHECKS=${DTF_CHECKS:-1}
+    DTF_URL=${DTF_URL:-https://github.com/h0tw1r3/dtf/raw/main/dtf.sh}
+    DTF_BRANCH=${DTF_BRANCH:-main}
     if [ -z "$DTF_REPO" ] ; then
         echo >&2 "${_DTF_FN}: DTF_REPO not set"
         return 1
     else
-        if [ ! -f "$_REPO/config" ] ; then
-            if _dtf init "$_REPO" >/dev/null ; then
-                _dtf_msg "init repo $_REPO"
-            else
+        if [ ! -f "${_DTF_WORKDIR}/config" ] ; then
+            if ! _dtf init "$_DTF_WORKDIR" >/dev/null ; then
                 _dtf_msg "failed to init repo"
                 return 1
             fi
-            _INIT=1
-            _CHECKS=1
+            _DTF_INIT=1
+            DTF_CHECKS=1
         fi
-        if [ "${_CHECKS}" -eq 1 ] ; then
+        if [ "${DTF_CHECKS}" -eq 1 ] ; then
             if [ "$(_dtf config --local --get status.showUntrackedFiles)" != "no" ] ; then
                 if ! _dtf config --local status.showUntrackedFiles no ; then
                     _dtf_msg "config set failed"
                     return 1
-                else
-                    _dtf_msg "config set"
                 fi
             fi
             if ! _dtf remote get-url origin >/dev/null 2>&1 ; then
                 if ! _dtf remote add origin "$DTF_REPO" ; then
                     _dtf_msg "failed to add remote origin $DTF_REPO"
                     return 1
-                else
-                    _dtf_msg "remote origin added"
                 fi
             fi
-            if [ ! -d "${_REPO}/refs/remotes/origin" ] ; then
+            if [ ! -d "${_DTF_WORKDIR}/refs/remotes/origin" ] ; then
                 if ! _dtf fetch origin ; then
                     _dtf_msg "failed to fetch remote"
                     return 1
@@ -65,24 +59,27 @@ dtf() {
                 echo ". ~/.${_DTF_FN}.sh" >> ~/.bashrc
             fi
         fi
-        if [ -n "${_INIT}" ] ; then
-            _dtf reset --hard "origin/${_BRANCH}"
+        if [ -n "${_DTF_INIT:-}" ] ; then
+            _dtf reset --hard "origin/${DTF_BRANCH}"
             _dtf submodule update --init --recursive
+            unset _DTF_INIT
         fi
-        _TREF=$(_dtf for-each-ref --format='%(upstream:short)' "$(_dtf symbolic-ref -q HEAD)")
-        if [ "${_TREF}" != "origin/${_BRANCH}" ] ; then
-            if ! _dtf branch -u "origin/${_BRANCH}" ; then
+        _DTF_TREF=$(_dtf for-each-ref --format='%(upstream:short)' "$(_dtf symbolic-ref -q HEAD)")
+        if [ "${_DTF_TREF}" != "origin/${DTF_BRANCH}" ] ; then
+            unset _DTF_TREF
+            if ! _dtf branch -u "origin/${DTF_BRANCH}" ; then
                 # TODO check if new repo
-                _dtf_msg "failed to track origin/${_BRANCH}, new repository?"
+                _dtf_msg "failed to track origin/${DTF_BRANCH}, new repository?"
                 return 1
             fi
             _dtf submodule update --recursive
         fi
+        unset _DTF_TREF
     fi
     if [ ! -f ~/."$_DTF_FN".sh ] ; then
         _dtf_msg "shell source is missing!? Please run '${_DTF_FN} upgrade' to install it again."
     elif [ "$*" = "upgrade" ] ; then
-        if _dtf_output_url "${_DTFURL}" ~/."$_DTF_FN".sh.$$ ; then
+        if _dtf_output_url "${DTF_URL}" ~/."$_DTF_FN".sh.$$ ; then
             # shellcheck source=/dev/null
             . ~/".$_DTF_FN".sh.$$ && \
                 cat ~/."$_DTF_FN".sh.$$ > ~/."$_DTF_FN".sh && \
